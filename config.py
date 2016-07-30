8956b0220e1c21a077c86ec9131bcbc341c2dc06
tux <me@johnpacific.com>
2016-07-30 17:00:21 -0600

    Add config details

---
+++ config.py
@@ -0,0 +1,7 @@
+from os import urandom
+from base64 import b64encode
+
+class Config():
+    SECRET_KEY = b64encode(urandom(66)).decode('utf-8')
+    SESSION_KEY_BITS = 256
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:test123@localhost/smirch'
