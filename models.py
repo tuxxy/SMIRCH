0716ee38ead0a92dd5da93d18a0cdd2abce833ad
tux <tux@xmission.com>
2016-08-04 12:56:00 -0600

    Update database models for prebought DIDs

--- models.py
+++ models.py
@@ -4,10 +4,20 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nick = db.Column(db.String(14), unique=True)
     user_phone = db.Column(db.String(20), unique=True)
-    user_did = db.Column(db.String(), unique=True)
-    is_subbed = db.Column(db.Boolean, default=False)
+    user_did = db.Relationship('DID', backref='user', lazy='dynamic')
+    is_subbed = db.Column(db.Boolean, default=True)
+    is_admin = db.Column(db.Boolean, default=False)
 
     def __init__(self, nick, user_phone, user_did):
         self.nick = nick
         self.user_phone = user_phone
         self.user_did = user_did
+
+
+class DID(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(20), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+     
+    def __init__(self, number):
+        self.number = number
