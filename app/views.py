aae34b9fecad1da15d5e19ef427f6a4112075786
tux <tux@xmission.com>
2016-08-04 14:32:52 -0600

    Add all the good stuff

--- app/views.py
+++ app/views.py
@@ -1,7 +1,30 @@
-from app import app
+from app import app, db
 from flask import request
+from api import Teli
+from models import User, DID
+import re
+
+teli = Teli(app.config['TELI_TOKEN'], app.config['TELI_DID'])
+
 
 @app.route('/', methods=['POST'])
 def main():
-    print(request.form.get('source'), request.form.get('message'))
-    return 'Hello!'
+    # subscribe <nick>
+    message = request.form.get('message').split(' ')
+    if message[0].lower() == 'subscribe' and len(message) >= 2:
+        subscribe_user(message, src)
+
+
+def subscribe_user(message, src):
+    nick = re.sub('[^A-Za-z0-9_-]+', '', message[1])
+    if len(nick >= 2):
+        # Grab the first available DID
+        did = DID.query.filter_by(user_id=None).first()
+        if did:
+            new_user = User(nick, src, did)
+            db.session.add(new_user)
+            db.session.commit()
+            teli.send_sms(int(src=new_user.did.number),
+                    int(new_user.user_phone),
+                    '''Welcome to Mojave SMS IRC for DEFCON 24!\n
+                    Written by @__tux for the Mojave!''')
--- models.py
+++ models.py
@@ -6,17 +6,19 @@ class User(db.Model):
     user_phone = db.Column(db.String(20), unique=True)
     is_subbed = db.Column(db.Boolean, default=True)
     is_admin = db.Column(db.Boolean, default=False)
-    user_did = db.relationship('DID', backref='user', lazy='dynamic')
+    did = db.relationship('DID', uselist=False, back_populates="user")
 
-    def __init__(self, nick, user_phone, user_did):
+    def __init__(self, nick, user_phone, did):
         self.nick = nick
         self.user_phone = user_phone
+        self.did = did
 
 
 class DID(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.String(20), unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    user = db.relationship("User", back_populates="did")
      
     def __init__(self, number):
         self.number = number
