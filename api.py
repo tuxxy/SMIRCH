469de86f39e603561736845c57e00e53bc49eb1a
tux <tux@xmission.com>
2016-08-04 14:31:17 -0600

    Oh, wow I'm a moron

--- api.py
+++ api.py
@@ -10,7 +10,9 @@ class Teli:
         self.SRC_DID = SRC_DID
         self.API = "https://sms.teleapi.net/{}/send"
 
-    def send_sms(self, dest, message, src=self.SRC_DID):
+    def send_sms(self, dest, message, src=None):
+        if src is None:
+            src = self.SRC_DID
         args = {
             'token': self.TOKEN,
             'source': src,
@@ -19,7 +21,9 @@ class Teli:
         }
         return requests.post(self.API.format("sms"), data=args).status_code
 
-    def send_mms(self, dest, file_name=None, file_data=None, file_url=None, src=self.SRC_DID):
+    def send_mms(self, dest, file_name=None, file_data=None, file_url=None, src=None):
+        if src is None:
+            src = self.SRC_DID
         args = {
             'token': self.TOKEN,
             'source': src,
