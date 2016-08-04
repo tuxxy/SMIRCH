1210f2b27a853ced9c9d4f297549b60a471f7d2a
tux <tux@xmission.com>
2016-08-04 13:19:33 -0600

    Add config entry for Teli API token

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config():
     SECRET_KEY = b64encode(urandom(66)).decode('utf-8')
     SESSION_KEY_BITS = 256
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:test123@localhost/smirch'
+    TELI_TOKEN = ""
