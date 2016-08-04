fbb21819890a5661bf9d72b5b158211c8e5a7461
tux <tux@xmission.com>
2016-08-04 12:59:40 -0600

    Fix models]

--- models.py
+++ models.py
@@ -4,14 +4,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nick = db.Column(db.String(14), unique=True)
     user_phone = db.Column(db.String(20), unique=True)
-    user_did = db.Relationship('DID', backref='user', lazy='dynamic')
     is_subbed = db.Column(db.Boolean, default=True)
     is_admin = db.Column(db.Boolean, default=False)
+    user_did = db.Relationship('DID', backref='user', lazy='dynamic')
 
     def __init__(self, nick, user_phone, user_did):
         self.nick = nick
         self.user_phone = user_phone
-        self.user_did = user_did
 
 
 class DID(db.Model):
