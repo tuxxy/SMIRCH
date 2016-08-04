ac7f184cced813bf2a7c714bb570a9b943a6c1f5
tux <tux@xmission.com>
2016-08-04 14:14:18 -0600

    Add SRC_DID to API

--- api.py
+++ api.py
@@ -1,14 +1,16 @@
 import requests
 
 class Teli:
-    TOKEN = ""
-    API = ""
+    TOKEN = None
+    SRC_DID = None
+    API = None
 
-    def __init__(self, TOKEN):
+    def __init__(self, TOKEN, SRC_DID):
         self.TOKEN = TOKEN
+        self.SRC_DID = SRC_DID
         self.API = "https://sms.teleapi.net/{}/send"
 
-    def send_sms(self, src, dest, message):
+    def send_sms(self, src=self.SRC_DID, dest, message):
         args = {
             'token': self.TOKEN,
             'source': src,
@@ -17,7 +19,7 @@ class Teli:
         }
         return requests.post(self.API.format("sms"), data=args).status_code
 
-    def send_mms(self, src, dest, file_name=None, file_data=None, file_url=None):
+    def send_mms(self, src=self.SRC_DID, dest, file_name=None, file_data=None, file_url=None):
         args = {
             'token': self.TOKEN,
             'source': src,
